318db51643d75523517cdf9ec884626c1fd4ccc1
AnetaDerkacz <derkacz.aneta@gmail.com>
2017-02-05 12:33:15 +0100

    nazwa

--- antybankrut/panel/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['logadm']);
-unset($_SESSION['user_nick']);
-unset($_SESSION['user_id']);
-unset($_SESSION['user_t']);
-header("Location: index.php");
-?>
--- antybankrut/panel/templates_c/%%B2^B29^B29B0B66%%right.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php /* Smarty version 2.6.28, created on 2017-02-04 22:59:52
-         compiled from blu/right.tpl */ ?>
